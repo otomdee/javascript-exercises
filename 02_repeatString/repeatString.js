e937b19f3abb14850b107434af60b756382eabe4
otomdee <otomdaniel65@gmail.com>
2024-07-03 15:13:33 +0100

    solve exercise 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+    if (num >= 1) {
+        let word = string;
+        for (let i = 0; i < (num - 1); i++) {
+            word += string;
+        }
+        return word
+    }
+    else if (num === 0 ) {
+        return ("")
+    }
+    else {
+        return ("ERROR")
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedStringArray = [];
+    for (let i = (string.length - 1); i >= 0; i--) {
+        reversedStringArray[(string.length - 1) - i] = splitString[i];
+    }
+    let reversedString = reversedStringArray.join("");
+    return(reversedString)
 };
 
 // Do not edit below this line
