b1950e316d08a9dcdd36f0540799d92442a507f0
otomdee <otomdaniel65@gmail.com>
2024-07-03 23:10:27 +0100

    solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = ((temp - 32) * (5/9)).toFixed(1);
+  return Number(newTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = ((temp * 9/5) + 32).toFixed(1);
+  return Number(newTemp);
 };
 
 // Do not edit below this line
