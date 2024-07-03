b06c56f3afb395a748a852f0e1ce44d3b1d2a853
otomdee <otomdaniel65@gmail.com>
2024-07-03 21:59:26 +0100

    solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,27 @@
 const sumAll = function() {
     const args = Array.from(arguments);
-    let first;
-    let second;
-    if (args[1] > args[0]) {
-       first = args[0];
-       second = args[1];
-     }
-     else if (args[0] >= args[1]) {
-       second = args[0];
-       first = args[1];
-     }
-    distance = (second - first);
+    if ((typeof(args[0]) == typeof(1) && typeof(args[1]) == typeof(1)) && (args[0] >= 0 && args[1] >= 0)) {
+        let first;
+        let second;
+        if (args[1] > args[0]) {
+            first = args[0];
+            second = args[1];
+        }
+        else if (args[0] >= args[1]) {
+            second = args[0];
+            first = args[1];
+        }
+        distance = (second - first);
+        let finalSum = first;
+    for (let i = 1; i <= distance; i++) {
+        finalSum += first + i;
+    }
 
-    let finalSum = first;
-   for (let i = 1; i <= distance; i++) {
-       finalSum += first + i;
-   }
-
-   return finalSum;
+    return finalSum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
