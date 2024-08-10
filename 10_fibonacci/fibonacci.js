480e8c846b9a0ba2fa17de2793e7c0ba38e3ee1f
otomdee <otomdaniel65@gmail.com>
2024-08-10 18:42:15 +0100

    solve exc 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(iterations) {
+    let fibArr = [1, 1];
+    if (iterations >= 3) {
+        for (let i = 3; i <= iterations; i++) {
+            fibArr.push(fibArr[i - 2] + fibArr[i - 3]);
+        }
+        return fibArr[iterations - 1];
+    }
+    else if (iterations == 1 || iterations == 2) {
+        return 1;
+    }
+    else if (iterations == 0) {
+        return 0;
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
