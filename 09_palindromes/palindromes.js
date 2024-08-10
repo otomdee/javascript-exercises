40425b918f2cc5bb1bb64088bfb27e275b5757e0
otomdee <otomdaniel65@gmail.com>
2024-08-10 18:12:01 +0100

    solve exc 8 and 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strFiltered = str.toUpperCase()
+    .split("")
+    .filter((item) => item !== " " && item !== "," && item !== "!" && item !== ".");
+    const strReversed = strFiltered.slice().reverse();
 
+    if (strFiltered.join("") == strReversed.join("")) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
